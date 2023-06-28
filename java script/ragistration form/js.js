fc5478b6b35b99166109d9c744cb7a387686f5b2
raxitrangadiya <raxitrangadiya8531@gmail.com>
2023-06-28 15:42:07 +0530

    validation req field

---
+++ java script/ragistration form/js.js
@@ -0,0 +1,24 @@
+//validation for this field is required
+let reqfields = document.getElementsByClassName("thisrequired")
+for (let index = 0; index < reqfields.length; index++) {
+    // console.log(reqfields);
+    reqfields[index].addEventListener("blur", (event) => {
+        console.log(event);
+        let txt = document.createElement("SPAN");
+        txt.innerHTML = "  *this field is required.."
+txt.style.color="red"
+        if (reqfields[index].value == "") {
+            if (reqfields[index].nextSibling == null) {
+                reqfields[index].parentNode.appendChild(txt)
+            }
+        } else {
+            if (reqfields[index].nextSibling != null) {
+                if (reqfields[index].nextSibling.nodeName == "SPAN") {
+                    reqfields[index].nextSibling.remove()
+                }
+            }
+        }
+    })
+}
+
+
